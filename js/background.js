e5a8d5f5407140faeba4316fcf3dbfd1d1427849
krozv <krozv.code@gmail.com>
2024-12-03 14:27:27 +0900

    settings: 프로젝트 셋팅

--- js/background.js
+++
@@ -1,8 +0,0 @@
-const chosenImage = Math.ceil(Math.random()*3); //3는 directory안에 있는 파일 개수를 자의로 입력한 것
-
-url = `img/bgimg${chosenImage}.jpg`; //img url
-
-const bgUrl = `"${url}"`
-const bgStyle = document.querySelector(".bgimg");
-
-document.body.style.backgroundImage = `url(${bgUrl})`;
